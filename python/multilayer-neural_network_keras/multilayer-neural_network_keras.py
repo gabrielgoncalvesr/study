9bb23ffea7f0646a114bd6533faf174d150ac214
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-07-13 23:46:56 -0300

    changing structure name

--- python/multilayer-neural_network_keras/multilayer-neural_network_keras.py
+++ python/multilayer-neural_network_keras/multilayer-neural_network_keras.py
@@ -1,4 +1,4 @@
-# importar os pacotes necessários
+# libraries
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -10,43 +10,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    # importar o MNIST
+    # importing MNIST
     print("[INFO] importando MNIST...")
     dataset = fetch_openml('mnist_784')
 
-    # normalizar todos pixels, de forma que os valores estejam
-    # no intervalor [0, 1.0]
+    # normalizing all pixels, to values stay in range [0, 1.0]
     data = dataset.data.astype("float") / 255.0
     labels = dataset.target
 
-    # dividir o dataset entre train (75%) e test (25%)
+    # dividing the dataset between train (75%) and test (25%)
     (trainX, testX, trainY, testY) = train_test_split(data, dataset.target)
 
-    # converter labels de inteiros para vetores
+    # converting labels de integers vectors
     lb = LabelBinarizer()
     trainY = lb.fit_transform(trainY)
     testY = lb.transform(testY)
 
-    # definir a arquitetura da Rede Neural usando Keras
+    # defining architecture of neural network using Keras
     # 784 (input) => 128 (hidden) => 64 (hidden) => 10 (output)
     model = Sequential()
     model.add(Dense(128, input_shape=(784,), activation="sigmoid"))
     model.add(Dense(64, activation="sigmoid"))
     model.add(Dense(10, activation="softmax"))
 
-    # treinar o modelo usando SGD (Stochastic Gradient Descent)
-    print("[INFO] treinando a rede neural...")
-    model.compile(optimizer=SGD(0.01), loss="categorical_crossentropy",
-                 metrics=["accuracy"])
-    H = model.fit(trainX, trainY, batch_size=128, epochs=100, verbose=2,
-             validation_data=(testX, testY))
+    # training the model using SGD (Stochastic Gradient Descent)
+    print("[INFO] training the neural network...")
+    model.compile(optimizer=SGD(0.01), loss="categorical_crossentropy", metrics=["accuracy"])
+    H = model.fit(trainX, trainY, batch_size=128, epochs=100, verbose=2, validation_data=(testX, testY))
 
-    # avaliar a Rede Neural
-    print("[INFO] avaliando a rede neural...")
+    # evaluating the neural network
+    print("[INFO] evaluating neural network...")
     predictions = model.predict(testX, batch_size=128)
     print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1)))
 
-    # plotar loss e accuracy para os datasets 'train' e 'test'
+    # plot loss and accuracy to datasets 'train' and 'test'
     plt.style.use("ggplot")
     plt.figure()
     plt.plot(np.arange(0,100), H.history["loss"], label="train_loss")
